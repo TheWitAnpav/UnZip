02f047d230c7c3e05fd39aeaf8d7772b909ac566
Anpav <thewitanpav@gmail.com>
2026-07-27 16:40:15 +0530

    Replace environment variables with hardcoded values
    Updated configuration values with hardcoded credentials.

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 
 class Config(object):
     # Mandotory
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    APP_ID = int(os.environ.get("35029722"))
+    API_HASH = os.environ.get("271cd4b0f1df86ba25f2568182ad2691")
+    BOT_TOKEN = os.environ.get("8277666643:AAFUkehS5w7n62ZkwEObye49t9B6O9a_vW4")
+    LOGS_CHANNEL = int(os.environ.get("-1003728144531"))
+    BOT_OWNER = int(os.environ.get("7192691902"))
     MONGODB_URL = os.environ.get("MONGODB_URL")
-    GOFILE_TOKEN = os.environ.get("GOFILE_TOKEN")
+    GOFILE_TOKEN = os.environ.get("Q6EqgbN0VmcVqbNgs17AiGTvn7fTxU3Z")
     # Optional
     MAX_DOWNLOAD_SIZE = int(os.environ.get("MAX_DOWNLOAD_SIZE")) if os.environ.get("MAX_DOWNLOAD_SIZE") else 10737418240
     # Constents
